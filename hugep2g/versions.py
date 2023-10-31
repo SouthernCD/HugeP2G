0866c3af3777d78f472a38877317ab066dcbcdd5
Yuxing Xu <xuyuxing14@mails.ucas.ac.cn>
2023-10-31 17:19:57 +0800

    1.0.1

--- hugep2g/versions.py
+++ hugep2g/versions.py
@@ -4,6 +4,12 @@ def get_versions():
 
 versions = [
     {
+        "number": "1.0.1",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
         "number": "1.0.0",
         "features": [
             "1. release",
